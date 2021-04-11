0147b4baa2ad89dc64931f9b54a57b568af14c19
Ivo Stoyanov <ivostoy@gmail.com>
2021-04-10 17:09:27 -0700

    segmentio evergeen-ui

---
+++ evergreen-hello/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Button } from 'evergreen-ui'
+
+// https://github.com/segmentio/evergreen
+
+ReactDOM.render(
+    <Button>I am using 🌲 Evergreen!</Button>,
+    document.getElementById('root')
+)
+
