84ecf1f045033582d4cdb9ffd5d1b74f2dd1e35d
Ivo Stoyanov <ivo@skyway-consulting.com>
2021-06-01 18:47:12 -0700

    WORKS, start player

--- mui-app/src/pages/Home/Home.js
+++ mui-app/src/pages/Home/Home.js
@@ -1,31 +1,45 @@
-import Page from 'material-ui-shell/lib/containers/Page'
 import React, {useState} from 'react'
-//import Scrollbar from 'material-ui-shell/lib/components/Scrollbar/Scrollbar'
-//import { useIntl } from 'react-intl'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Page from 'material-ui-shell/lib/containers/Page'
 import Clips from '../../components/Clips/Clips'
 import ClipEvents from '../../components/Clips/ClipEvents'
+/*
+    Based on  https://www.react-most-wanted.com and https://material-ui.com
+ */
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 
 const HomePage = () => {
-    // eslint-disable-next-line no-unused-vars
   const [selectedClip, setSelectedClip] = useState(null);
-  //const intl = useIntl()
+  const classes = useStyles();
 
   return (
       <Page>
-        <Clips setSelectedClip={setSelectedClip}/>
-        <ClipEvents selectedClip={selectedClip}/>
+          <Grid container spacing={3}>
+              <Grid item xs={8}>
+                  <Paper className={classes.paper}>
+                      <Clips setSelectedClip={setSelectedClip}/>
+                  </Paper>
+              </Grid>
+              <Grid item xs={4}>
+                  <Paper className={classes.paper}>
+                      <ClipEvents selectedClip={selectedClip}/>
+                  </Paper>
+              </Grid>
+          </Grid>
       </Page>
   )
 }
 
 export default HomePage
-
-// <Page pageTitle={intl.formatMessage({ id: 'home' })}>
-//   <Scrollbar
-//     style={{ height: '100%', width: '100%', display: 'flex', flex: 1 }}
-//   >
-//     {intl.formatMessage({ id: 'home' })}
-//   </Scrollbar>
-//
-//
-// </Page>
